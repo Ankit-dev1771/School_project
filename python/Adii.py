245fb6270ce13c3d3d8716d876bcaac47396b398
Ankit-dev1771 <ankitsingh2481@gmail.com>
2024-11-22 07:18:53 +0530

    Update Adii.py

--- python/Adii.py
+++ python/Adii.py
@@ -1,116 +1,118 @@
 import pymysql as myq
-print(""
-_____________________________________
-welcome to medical management system
-_____________________________________"")
-con=myq.connect(host="localhost",user="root",passwd="",database="")
-if con is connected:
-	print("connected successfully")
+
+print("Welcome to the Medical Management System")
+
+# Establish database connection
+con = myq.connect(host="localhost", user="root", passwd="", database="")
+if con.open:
+    print("Connected successfully")
 else:
-	print("error")
-mycursor=con.cursor()
-mycursor.execute("create database if not exists sales_vdo")
-mycursor.execute("use sales_vdo")
-mycursor.execute("create table if not exists login(username varchar(25) not null,password varchar(25) not null")
-mycursor.execute("create table if not exists purchase(odate date not null, name varchar(25) not null,pcode int not null,amount int not null)")
-mycursor.execute("create table if not exists stock(pcode int not null,pname varchar(25) not null, quantity int not null, price int not null) ")
+    print("Error")
+
+mycursor = con.cursor()
+
+# Create database and tables if not exist
+mycursor.execute("CREATE DATABASE IF NOT EXISTS sales_vdo")
+mycursor.execute("USE sales_vdo")
+
+# Fix missing closing parenthesis in the login table creation statement
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS login (
+        username VARCHAR(25) NOT NULL,
+        password VARCHAR(25) NOT NULL
+    )
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS purchase (
+        odate DATE NOT NULL,
+        name VARCHAR(25) NOT NULL,
+        pcode INT NOT NULL,
+        amount INT NOT NULL
+    )
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock (
+        pcode INT NOT NULL,
+        pname VARCHAR(25) NOT NULL,
+        quantity INT NOT NULL,
+        price INT NOT NULL
+    )
+""")
+
 con.commit()
-z=0
-mycursor.execute("select * from login")
-for i in mycursor:
-	z+=1
-if(z==0):
-	mycursor.execute("insert into login values('username','aditya')")
-	con.commit()
+
+# Initialize login check
+z = 0
+mycursor.execute("SELECT * FROM login")
+for _ in mycursor:
+    z += 1
+
+# Insert default admin if no records exist
+if z == 0:
+    mycursor.execute("INSERT INTO login (username, password) VALUES ('admin', 'aditya')")
+    con.commit()
+
+# Main menu
 while True:
-	print(""""1.Admin2.Customer3.Exit""")
-	ch=int(input("enter your choice:"))
-	if(ch==1):
-		passs=input("enter password:")
-		mycursor.execute("select * from login")
-		for i in mycursor:
-			username,password=i
-		if(passs==password):
-			print("welcome")
-			loop2='y'
-			while(loop2=='y' or loop2=='Y'):
-				print("1.Add new item 2.updating price 3.deleting item 4.display all items 5. To change the password 6.Log out")
-				ch=int(input("enter your choice:"))
-				if(ch==1):
-					loop='y'
-					while(loop=='y' or loop=='Y'):
-						pcode=int(input("enter product code:")
-						pname=input("enter product name:")
-						quantity=int(input("enter product quantity:"))
-						price=int(input("enter product price:"))
-						mycursor.execute("insert into stock values('"+str(pcode)+"','"+pname+"','"+str(quantity)+"','"+str(price)+"')")
-						con.commit()
-						print("record successfully inserted..")
-						loop=input("do you want to enter more items(y/n)")
-					loop2=input("do you want to continue editing stock(y/n):")
-				elif(ch==2):
-					loop='y'
-					while(loop=='y' or loop=='Y'):
-						pcode=int(input("enter the product code:"))
-						new_price=int(input("enter the new price:"))
-						mycursor.execute("update stock set price='"+str(new_price)+"' where pcode='"+str(pcode)+"'")
-						con.commit()
-						loop=input("do you want to change price of any other item(y/n):")
-					loop2=input("do you want to continue editing stock(y/n):")
-				elif(ch==3):
-						loop='y'
-						while(loop=='y' or loop=='Y'):
-							pcode=int(input("enter the product code:"))
-							mycursor.execute("delete from stock where pcode='"+str(pcode)"'")
-							con.commit()
-							loop=input("do you want to delete any other data(y/n):")
-						loop2=input("do you want to continue editing stock(y/n):")
-				elif(ch==4):
-						mycursor.execute("select * from stock")
-						print("pcode || pname || quantity || price")
-						for i in mycursor:
-							t_code,t_name,t_quan,t_price=i
-							print(f"{t_code}|| {t_name} || {t_quan}|| {t_price}")
-						loop2=input("do you want to continue editing stock(y/n):")
-				 elif(ch==5):
-				 	old_passs=input("enter the old password :")
-				 	mycursor.execute("select * from login")
-				 	for i in mycursor:
-				 		username,password=i
-				 	if(old_passs==password):
-				 		new_passs=input("enter the new password:")
-				 		mycursor execute("update login set password='"+new_passs+"'")
-				 		con.commit()
-				 		print("password changes successfully..")
-				 	loop2=input("do you want to continue editing stock(y/n):")
-				 elif(ch==6):
-				 	break
-	else:
-		print("wrong password")
-	elif(ch==2):
-				 	print("""1.item bucket 2.payment 3.view available items 4.go back""")
-				 	ch2=int(input("enter your choice: "))
-				 	if(ch2==1):
-				 		name=input("enter your name:")
-				 		pcode=int(input("enter product code:"))
-				 		quantity=int(input("enter product quantity:"))
-				 		mycursor.execute("select * from stock where pcode='"+str(pcode)+"'")
-				 		for i in mycursor:
-				 			t_code,t_name,t_quan,t_price=i
-				 		amount=t_price*quantity
-				 		net_quan=t_quan-quantity
-				 		mycursor.execute("update stock set quantity='"+str(net_quan)+"' where pcode='"+str(pcode)+"'")
-				 		mycursor.execute("insert into purchase values(now(),'"+name+"','"+str(pcode)"','"+str(amount)+"'")
-				 		con.commit()
-				 	elif(ch2==2):
-				 			print(f"amount to be paid {amount}")
-				 	elif(ch2==3):
-				 			print("code || name || price")
-				 			mycursor.execute("select * from stock")
-				 			for i in mycursor:
-				 				t_code,t_name,t_quan,t_price=i
-				 				print(f"{t_code} || {t_name} || {t_price}")
-				 	elif(ch2==4):
-				 		break
-	elif(ch==3):
-				 		break
+    print("""1. Admin
+2. Customer
+3. Exit""")
+    ch = int(input("Enter your choice: "))
+
+    if ch == 1:  # Admin section
+        password_input = input("Enter password: ")
+        mycursor.execute("SELECT * FROM login")
+        username, password = mycursor.fetchone()  # Assuming only one admin user
+
+        if password_input == password:
+            print("Welcome Admin")
+            loop2 = 'y'
+            while loop2.lower() == 'y':
+                print("""
+                1. Add new item
+                2. Update price
+                3. Delete item
+                4. Display all items
+                5. Change password
+                6. Log out
+                """)
+                ch = int(input("Enter your choice: "))
+
+                if ch == 1:  # Add new item
+                    loop = 'y'
+                    while loop.lower() == 'y':
+                        pcode = int(input("Enter product code: "))
+                        pname = input("Enter product name: ")
+                        quantity = int(input("Enter product quantity: "))
+                        price = int(input("Enter product price: "))
+                        mycursor.execute("""
+                            INSERT INTO stock (pcode, pname, quantity, price)
+                            VALUES (%s, %s, %s, %s)
+                        """, (pcode, pname, quantity, price))
+                        con.commit()
+                        print("Record successfully inserted.")
+                        loop = input("Do you want to enter more items (y/n): ")
+                    loop2 = input("Do you want to continue editing stock (y/n): ")
+
+                elif ch == 2:  # Update price
+                    loop = 'y'
+                    while loop.lower() == 'y':
+                        pcode = int(input("Enter the product code: "))
+                        new_price = int(input("Enter the new price: "))
+                        mycursor.execute("""
+                            UPDATE stock SET price = %s WHERE pcode = %s
+                        """, (new_price, pcode))
+                        con.commit()
+                        loop = input("Do you want to change price of any other item (y/n): ")
+                    loop2 = input("Do you want to continue editing stock (y/n): ")
+
+                elif ch == 3:  # Delete item
+                    loop = 'y'
+                    while loop.lower() == 'y':
+                        pcode = int(input("Enter the product code: "))
+                        mycursor.execute("DELETE FROM stock WHERE pcode = %s", (pcode,))
+                        con.commit()
+                        loop = input("Do you want to delete any other data (y/n): ")
+                    loop2 = input("Do you want to continue editing stock (y/n): "
